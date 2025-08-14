0a69e352cb56c50d7c248564f133764ebc1848ae
Zarin Rashid <zarinspeaks@gmail.com>
2025-08-14 11:38:52 +0600

    Add WalletConnectButton component and update dependencies

---
+++ src/component/Button/WalletConnectButton.jsx
@@ -0,0 +1,32 @@
+"use client";
+import { Button } from "@mui/material";
+import { ethers } from "ethers";
+import { useState } from "react";
+
+export default function WalletConnectButton() {
+  const [address, setAddress] = useState(null);
+
+  async function onClickHandler() {
+    if (!window.ethereum) {
+      alert("Please install Metamask");
+      return;
+    }
+    try {
+      const provider = new ethers.BrowserProvider(window.ethereum);
+      const accounts = await provider.send("eth_requestAccounts", []);
+      setAddress(accounts[0]);
+    } catch (err) {
+      console.error("User rejected connection", err);
+    }
+  }
+
+  return (
+    <main className="flex justify-center items-center h-screen">
+      <Button variant="outlined" onClick={onClickHandler}>
+        {address
+          ? `${address.slice(0, 6)}...${address.slice(-4)}`
+          : "Connect Wallet"}
+      </Button>
+    </main>
+  );
+}
