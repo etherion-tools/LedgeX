2649e97e5e2340665b06de424b0a87f7fdb0befc
Zarin Rashid <zarinspeaks@gmail.com>
2025-08-18 05:54:14 -0600

    Refactor Header and WalletConnectButton components to support wallet connection and display wallet address

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,8 +1,21 @@
+"use client";
+import Header from "@/component/Header/Header";
 import ReusableCard from "@/component/ReusableCard/reusablecard";
+import { useState } from "react";
 
 export default function Page() {
+  const [walletAddress, setWalletAddress] = useState<string | undefined>();
+
+  function onConnect(address: string) {
+    setWalletAddress(address);
+  }
   return (
-    <div className='flex justify-center items-center min-h-screen bg-foreground'>
+    <div className="flex justify-center items-center min-h-screen bg-foreground">
+      <Header
+        title="Test Page"
+        walletAddress={walletAddress || ""}
+        onConnect={onConnect}
+      />
       <ReusableCard name="TestXXX" walletAddress="abcdxxxx0000" />
     </div>
   );
