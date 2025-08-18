86436b60b89e5303c0b572a63c948c0e2f27f0e3
Zarin Rashid <zarinspeaks@gmail.com>
2025-08-18 15:13:43 +0600

    Implement Dashboard Layout Structure
    Co-authored-by: Ayesha Khan <ayeshkhan.eth@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
-export default function Page() {
+"use client";
+
+import TabbedNavigation from "@/component/TabbedNavigation";
+import TransactionsCharts from "@/component/TransactionsCharts";
+import WalletCards from "@/component/WalletCards";
+
+
+export default function Page(){
   return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-50 dark:bg-gray-900">
+   <main className="flex min-h-screen items-center justify-center bg-gray-50 dark:bg-gray-900">
       <h1 className="text-4xl font-bold text-gray-900 dark:text-white">
         LedgerX Dashboard
       </h1>
+      <WalletCards />
+      <TabbedNavigation />
+      <TransactionsCharts />
     </main>
   );
 }
